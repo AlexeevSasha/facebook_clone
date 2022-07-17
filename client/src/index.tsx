f8d302b69a4597aceb36b2684d872ea856cf3ab7
work.alexeevs@gmail.com <work.alexeevs@gmail.com>
2022-07-17 20:44:33 +0300

    client init

---
+++ client/src/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {ThemeProvider} from "styled-components";
+import App from './App';
+
+//styles
+import {GlobalStyles} from "./styles/GlobatStyles";
+import {theme} from "./styles/theme";
+
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+root.render(
+    <React.StrictMode>
+        <GlobalStyles/>
+        <ThemeProvider theme={theme}>
+            <App/>
+        </ThemeProvider>
+    </React.StrictMode>
+);
