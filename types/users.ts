c2d7e5b945c060c4f80ab09d4a33aa344e3529aa
Alexeev Alexander <work.alexeevs@gmail.com>
2022-07-18 16:02:52 +0300

    types users

---
+++ types/users.ts
@@ -0,0 +1,19 @@
+export interface IUsersModel {
+  id: string;
+  firstname: string;
+  lastname: string;
+  username: string;
+  email: string;
+  password: string;
+  picture?: string;
+  cover?: string;
+  gender?: string;
+  bYear?: number;
+  bMonth?: number;
+  bDay?: number;
+  verified?: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export type IUser = Omit<IUsersModel, 'id' | 'createdAt' | 'updatedAt'>;
