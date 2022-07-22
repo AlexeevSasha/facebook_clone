b5e32e9cc856d7018c8f7adbd9a1fe57b56929b5
Alexeev Alexander <work.alexeevs@gmail.com>
2022-07-22 10:16:31 +0300

    login and activate mail

--- controllers/authControllers.ts
+++ controllers/authControllers.ts
@@ -1,16 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 import { IUser } from '../types/users';
 import Users from '../models/Users';
 import { emailValidate, lengthValidate } from '../helpers/validation';
-import { BadRequestError } from '../errors';
 import { generateToken } from '../helpers/generateToken';
 import { sendVerificationEmail } from '../helpers/mailing';
+import { BadRequestError, UnauthenticatedError } from '../errors';
 
-export const register = async (
-  req: Request<{}, {}, IUser>,
-  res: Response,
-  next: NextFunction
-) => {
+
+
+export const register = async (req: Request<{}, {}, IUser>, res: Response, next: NextFunction) => {
   try {
     const { email, firstname, lastname, password } = req.body;
 
@@ -24,9 +24,7 @@ export const register = async (
     }
 
     if (!lengthValidate(password, 3, 30)) {
-      throw new BadRequestError(
-        'Password must be at least 3 and not more than 30 characters'
-      );
+      throw new BadRequestError('Password must be at least 3 and not more than 30 characters');
     }
 
     const user = await Users.create({
@@ -43,6 +41,47 @@ export const register = async (
     const token = generateToken({ id: user.id }, '7d');
 
     res.send({ user, token });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const activateEmail = async (req: Request<{}, {}, {token: string}>, res: Response, next: NextFunction) => {
+  try {
+    const { token } = req.body;
+    const user = jwt.verify(token, process.env.JWT_SECRET as string);
+
+    if (typeof user === 'string') {
+      throw new BadRequestError('User nod found');
+    }
+
+    const check = await Users.findOne({ where: { id: user.id } });
+    if (check && check.verified) {
+      throw new BadRequestError('This email is already activated');
+    } else {
+      return res.status(200).json({ message: "Account has beeen activated successfully." });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+export const login = async (req: Request<{}, {}, {email: string, password: string}>, res: Response, next: NextFunction) => {
+  try {
+    const {email, password} = req.body;
+
+    const user = await Users.scope('withPassword').findOne({ where: { email } });
+    if (!user) {
+      throw new UnauthenticatedError('The email address you entered does not exist');
+    }
+
+    const checkPassword = await bcrypt.compare(password, user.password);
+    if (!checkPassword) {
+      throw new UnauthenticatedError('Invalid password')
+    }
+
+    res.send(user)
   } catch (error) {
     next(error);
   }
