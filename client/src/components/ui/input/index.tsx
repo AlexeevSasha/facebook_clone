0971b643de58fb8fafd7bb72c12fbc65940f6162
Alexeev Alexander <work.alexeevs@gmail.com>
2022-07-25 16:15:04 +0300

    input password

--- client/src/components/ui/input/index.tsx
+++ client/src/components/ui/input/index.tsx
@@ -1,5 +1,5 @@
 import TextField from '@mui/material/TextField';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 interface IProps {
   id: string;
@@ -13,4 +13,20 @@ interface IProps {
 
 export const Input: FC<IProps> = ({ id, label, variant, type, placeholder, name, fullWidth }) => {
   return <TextField name={name} placeholder={placeholder} id={id} label={label} variant={variant || 'outlined'} fullWidth={fullWidth} type={type} />;
+};
+
+export const InputPassword: FC<Omit<IProps, 'type'>> = ({ id, label, variant, placeholder, name, fullWidth }) => {
+  const [showPass, setShowPass] = useState(false);
+  const toggleShow = () => setShowPass(!showPass);
+  return (
+    <TextField
+      name={name}
+      placeholder={placeholder}
+      id={id}
+      label={label}
+      variant={variant || 'outlined'}
+      fullWidth={fullWidth}
+      type={showPass ? 'text' : 'password'}
+    />
+  );
 };
