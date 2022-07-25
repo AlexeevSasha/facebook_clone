6ee8885f5bc27480bb1233f726987169af2d4b07
Alexeev Alexander <work.alexeevs@gmail.com>
2022-07-25 16:14:51 +0300

    add login form

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { FC } from 'react';
-import { Input } from '../components/ui/input';
+import { LoginForm } from '../components/AuthForm/LoginForm';
 
 export const SignIn: FC = () => {
   return (
-    <div style={{ margin: 100 }}>
-      <Input id="1" label="Test" name="email" />
+    <div>
+      <LoginForm />
     </div>
   );
 };
